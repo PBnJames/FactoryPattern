457cc560e7e189adacb34a1dfd216e4ac78ae734
James Moore <pbnjames@jamess-mbp.lan>
2018-09-23 17:05:44 -0400

    initial commit

---
+++ src/ChicagoPizzaStore.java
@@ -0,0 +1,10 @@
+public class ChicagoPizzaStore extends PizzaStore {
+    @Override
+    protected Pizza createPizza(String item) {
+        if(item.equals("cheese")){
+            return new ChicagoStyleCheesePizza();
+        } else{
+            return null;
+        }
+    }
+}
